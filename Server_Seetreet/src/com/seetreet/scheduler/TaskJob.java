2e303dbc8745b5ba24e50fe2f9d255a416eea262
seongjun1227 <seongjun1227@naver.com>
2014-10-16 00:46:35 +0900

    141016_add to quartz

---
+++ Server_Seetreet/src/com/seetreet/scheduler/TaskJob.java
@@ -0,0 +1,34 @@
+package com.seetreet.scheduler;
+
+import com.seetreet.dao.MongoDAO;
+import com.seetreet.dao.MongoDB;
+
+import com.seetreet.http.HttpCall;
+import com.seetreet.http.HttpControl;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.mongodb.DBObject;
+
+public class TaskJob implements Job {
+	
+	public void execute(JobExecutionContext context) throws JobExecutionException{
+		System.out.println("-------------------------------------");
+		System.out.println("TaskJob executed");
+		/*
+		if(MongoDAO.isTaskData()){
+			MongoDAO.updateTaskTime();
+			List<DBObject> list = MongoDAO.taskTimeCheck();
+			//System.out.println(list.size());
+			//Todo list in check
+			if(list!=null&&list.size()>0){
+				MongoDAO.insertQueue(list);
+			}
+		}
+		*/
+		System.out.println("TaskJob end : ");
+		System.out.println("-------------------------------------\n");
+		
+	}
+}
---
+++ Server_Seetreet/src/com/seetreet/scheduler/schedulerServlet.java
@@ -0,0 +1,49 @@
+package com.seetreet.scheduler;
+
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+/**
+ * Servlet implementation class schedulerServlet
+ */
+@WebServlet("/schedulerServlet")
+public class schedulerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public void init(){
+    	try{
+    		SchedulerFactory schedFact = new StdSchedulerFactory();
+    		Scheduler sched = schedFact.getScheduler();
+
+   		 	sched.start();
+   		
+   			JobDetail taskJob = org.quartz.JobBuilder.newJob(TaskJob.class)
+   				.withIdentity("taskJob", "group1")
+   				.build();
+   			Trigger trigger = org.quartz.TriggerBuilder.newTrigger()
+   				.withIdentity("trigger", "group1")
+   				.startNow()
+   				.withSchedule(SimpleScheduleBuilder.simpleSchedule()
+   						.withIntervalInSeconds(5)	
+   						.repeatForever())            
+   						.build();
+   			
+ 		
+   		sched.scheduleJob(taskJob, trigger);
+
+    	}catch(SchedulerException e){
+    		e.printStackTrace();
+    	}
+    	
+    }
+
+}
