114689a5efd1d4223e033e2cdc0b359fa7230f9c
seongjun1227 <seongjun1227@naver.com>
2014-11-18 00:41:15 +0900

    aaa

--- Server_Seetreet/src/com/seetreet/util/C.java
+++ Server_Seetreet/src/com/seetreet/util/C.java
@@ -22,9 +22,9 @@ public class C {
 	public static final String CONTENTTYPE_CSS  = "text/css";
 		
 	public static final String APISERVER = "http://api.visitkorea.or.kr/openapi/service/rest/KorService";
-	public static final String SERVICEKEY = "?ServiceKey=%2F6Zn8OJhDvfGgLf7GPednHRsj5iW669wUrfdrURKt%2FKvpUw%2F1PYsnTL75PFHlZ%2BgU4mCP0qiN74SV%2F6NMM8M%2Fg%3D%3D";
+	//public static final String SERVICEKEY = "?ServiceKey=%2F6Zn8OJhDvfGgLf7GPednHRsj5iW669wUrfdrURKt%2FKvpUw%2F1PYsnTL75PFHlZ%2BgU4mCP0qiN74SV%2F6NMM8M%2Fg%3D%3D";
 	//public static final String SERVICEKEY = "?ServiceKey=x9silkNV5NAwSY11q%2BB3wEIwMLN%2Bk41GRMIMMHseSBI%2BZZJacUaPhAAUno73xVl//xg/6jq7ximTUaUY1NkL3Q%3D%3D";
-	//public static final String SERVICEKEY = "?ServiceKey=%2Bjvgyj%2FpkZ2s2LRybZLnbB2FMv2i8PvJlK1%2F%2FNLL8KXwqo9Ian%2FUXJciP9MKJFqlqQkvnQaO9Xqyw6dC955xTg%3D%3D";
+	public static final String SERVICEKEY = "?ServiceKey=%2Bjvgyj%2FpkZ2s2LRybZLnbB2FMv2i8PvJlK1%2F%2FNLL8KXwqo9Ian%2FUXJciP9MKJFqlqQkvnQaO9Xqyw6dC955xTg%3D%3D";
 	
 
 	public static final String DETAILCOMMON = "/detailCommon";
